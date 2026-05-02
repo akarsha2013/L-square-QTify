9e9fd4aaecaa18d67a5f606faa363e845c6246ac
Akarsha <akarshaaku6@gmail.com>
2026-05-02 16:44:34 +0530

    fix: corrected API endpoint and tests issues

--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -1,40 +1,33 @@
 import axios from "axios";
-export const BACKEND_ENPOINT="https://qtify-backend-labs.crio.do";
 
-// function to get the Top-albums data from backend api
-export const fetchTopAlbums=async()=>{
-    try{
-        const res= await axios.get(`${BACKEND_ENPOINT}/albums/top`);
-        // console.log(res.data);
-        return res.data;
-    }
-    catch(error){
-        // console.log(error);
-        return null;
-    }
-    
-}
+export const BACKEND_ENDPOINT = "https://qtify-backend.labs.crio.do";
 
-// function to get the New-albums data from backend api
-export const fetchNewAlbums=async()=>{
-    try{
-        const res= await axios.get(`${BACKEND_ENPOINT}/albums/new`);
-        console.log(res.data);
-        return res.data;
-    }
-    catch(error){
-        console.log(error);
-        return null;
-    }
-    
-}
+// Top albums
+export const fetchTopAlbums = async () => {
+  try {
+    const res = await axios.get(`${BACKEND_ENDPOINT}/albums/top`);
+    return res.data;
+  } catch (error) {
+    return null;
+  }
+};
 
-export const fetchSongs=async()=>{
-    try{
-        const res=await axios.get(`${BACKEND_ENPOINT}/songs`)
-        return res.data;
-    }
-    catch(error){
-        return null;
-    }
-}
+// New albums
+export const fetchNewAlbums = async () => {
+  try {
+    const res = await axios.get(`${BACKEND_ENDPOINT}/albums/new`);
+    return res.data;
+  } catch (error) {
+    return null;
+  }
+};
+
+// Songs
+export const fetchSongs = async () => {
+  try {
+    const res = await axios.get(`${BACKEND_ENDPOINT}/songs`);
+    return res.data;
+  } catch (error) {
+    return null;
+  }
+};
